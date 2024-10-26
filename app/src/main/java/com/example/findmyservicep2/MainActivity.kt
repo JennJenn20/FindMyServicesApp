6cfa1682c446adefbb039deca1e4f8bb8f3688f7
ST10153606 <129886294+ST10153606@users.noreply.github.com>
2024-10-25 20:37:38 -0800

    3rd commit

--- app/src/main/java/com/example/findmyservicep2/MainActivity.kt
+++ app/src/main/java/com/example/findmyservicep2/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.example.findmyservicep2
-
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Find the button by its ID
+        val buttonMoveToLoginPage: Button = findViewById(R.id.MovetoLoginPage)
+
+// Set up an OnClickListener for the button
+        buttonMoveToLoginPage.setOnClickListener {
+            // Create an Intent to start the new activity
+            val intent = Intent(this, LoginActivity::class.java)
+            // Start the new activity
+            startActivity(intent)
+        }
+
+        // Find the button by its ID
+        val buttonMoveToSignUpPage: Button = findViewById(R.id.MovetoSignUpPage)
+
+// Set up an OnClickListener for the button
+        buttonMoveToSignUpPage.setOnClickListener {
+            // Create an Intent to start the new activity
+            val intent = Intent(this, SignUpActivity::class.java)
+            // Start the new activity
+            startActivity(intent)
+        }
+
     }
 }
--- app/src/main/java/com/example/findmyservicep2/SignUpActivity.kt
+++ app/src/main/java/com/example/findmyservicep2/SignUpActivity.kt
@@ -1,4 +1,26 @@
 package com.example.findmyservicep2
 
-class SignUpActivity {
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class SignUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.signup_activity)
+
+        // Find the button by its ID
+        val buttonMoveBackToGetStartedPage: Button = findViewById(R.id.btnbacktogetstarted)
+
+// Set up an OnClickListener for the button
+//
+        buttonMoveBackToGetStartedPage.setOnClickListener {
+            // Create an Intent to start the new activity
+            val intent = Intent(this, MainActivity::class.java)
+            // Start the new activity
+            startActivity(intent)
+        }
+    }
 }
